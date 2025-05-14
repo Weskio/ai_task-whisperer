4ea4c1b159b706a105ea967541fa00cbc1b1f893
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-05-14 14:43:14 +0000

    fix: speed insights import error

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -25,7 +25,7 @@ const App = () => (
       </TooltipProvider>
     </QueryClientProvider>
     <Analytics />
-    <SpeedInsights />
+    <SpeedInsights/>
   </>
 );
 
